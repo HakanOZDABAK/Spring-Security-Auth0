bc47cedc5be967de9de27c4c677ca48142336509
HakanOZDABAK <hakanozdabak2000@hotmail.com>
2024-01-30 13:42:19 +0300

    initialized project

---
+++ src/main/java/com/hakanozdabak/springsecurityauth0/SpringSecurityAuth0Application.java
@@ -0,0 +1,13 @@
+package com.hakanozdabak.springsecurityauth0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAuth0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityAuth0Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/hakanozdabak/springsecurityauth0/SpringSecurityAuth0ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hakanozdabak.springsecurityauth0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAuth0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
