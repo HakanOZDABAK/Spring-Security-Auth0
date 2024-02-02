95ac7209d866ab5e99f43254497df8de6abec341
HakanOZDABAK <hakanozdabak2000@hotmail.com>
2024-02-02 13:45:04 +0300

    added github oauth2

--- src/main/java/com/hakanozdabak/springsecurityauth0/config/SecurityConfig.java
+++ src/main/java/com/hakanozdabak/springsecurityauth0/config/SecurityConfig.java
@@ -1,11 +1,10 @@
 package com.hakanozdabak.springsecurityauth0.config;
 
 
-import com.hakanozdabak.springsecurityauth0.exception.OAuth2LoginSuccessHandler;
-import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,41 +14,42 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
 import java.util.List;
-
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
 
-    @Autowired
-    private OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
+
+
+    @Value("${frontend.url}")
+    private String frontendUrl;
+
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws  Exception{
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-                .authorizeHttpRequests(auth->auth.anyRequest().authenticated())
-                .oauth2Login(oath2->{
+                .authorizeHttpRequests(auth -> {
+                    auth.anyRequest().authenticated();
+                })
 
-                    oath2.successHandler(oAuth2LoginSuccessHandler);
-                }).build();
+                .oauth2Login(oath2 -> {
 
-
-
-
+                })
+                .build();
     }
 
     @Bean
-    CorsConfigurationSource corsConfigurationSource(){
+    CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000","http://127.0.0.1:3000"));
-        configuration.setAllowedMethods(Arrays.asList("GET","PUT","DELETE","POST"));
-        configuration.setAllowedHeaders(List.of("Authorization","Accept","Content-Type","method","redirect"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**",configuration);
-        return source;
+        configuration.setAllowedOrigins(List.of(frontendUrl));
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", configuration);
+        return urlBasedCorsConfigurationSource;
     }
 }
--- src/main/java/com/hakanozdabak/springsecurityauth0/controller/MessageController.java
+++ src/main/java/com/hakanozdabak/springsecurityauth0/controller/MessageController.java
@@ -1,6 +1,7 @@
 package com.hakanozdabak.springsecurityauth0.controller;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,12 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/message")
-@CrossOrigin
+
 public class MessageController {
 
-
+    @Value("${frontend.url}")
+    private String frontendUrl;
     @GetMapping("/hello")
     public String helloMessage(){
-        return "Hello Guys";
+        return frontendUrl;
     }
+
 }
